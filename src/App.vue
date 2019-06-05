e21eabbf2639ceeed38ba38c0c2afea14e957160
liuxinlong <lxl740895804@126.com>
2019-06-05 20:57:50 +0800

    建立项目

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+     <div id="wrap">
+      <div id="text">
+        <div id="code"> <font color="#FF0000"> <span class="say">Hai，girl,虽然你看不见我对你的告白</span><br>
+          <span class="say"> </span><br>
+          <span class="say"> 我知道我不会甜言蜜语，但是我会用行动证明一切</span><br>
+          <span class="say"> </span><br>
+          <span class="say">你说我啰嗦，有些东西因为越在乎所以越失去</span><br>
+          <span class="say">因为我太爱你了，所以我失去你了</span><br>
+          <span class="say">因为我太啰嗦了，所以你离开了我</span><br>
+          <span class="say">虽然你看不见我的告白，但是我还是要把这些心里话说出来</span><br>
+          <span class="say">每一天都是伴随着对你的思念入睡，只要想到你就会有种莫名的开心。每次的走神都是想你、想你、想你，梦想着与你在一起的每一天，我会宠你、爱你、疼你、在乎你的一切，更会顺着你、体贴你、关心你</span><br>
+          <span class="say"> </span><br>
+          <span class="say"> 蝴蝶每天眨几次眼睛？煽动几次翅膀？夜空有多少颗明星？又有多少颗会陨落？这些你统统都不知道，也不需要知道，你只需要知道有个人时时刻刻都在关注着你，思念着你</span><br>
+          <span class="say"><span class="space"></span> -- 爱你的XXX--</span> </font>
+        </div>
+      </div>
+      <div id="clock-box"> <span class="STYLE1"></span><font color="#33CC00">还有一点你不知道，我喜欢你</font> <span class="STYLE1">已经是……</span>
+        <div id="clock"></div>
+      </div>
+      <canvas id="canvas" width="1100" height="680"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app{
+  width: 100%;
+}
+</style>
